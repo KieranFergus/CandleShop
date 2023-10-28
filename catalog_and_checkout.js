51a63f9c2a6dd7f2c261f58fd6cfe0d6abab45a3
ermiayn3962 <yodahie.ermias@gmail.com>
2023-10-28 08:45:56 -0400

    Finished checkout w/o images

--- catalog_and_checkout.js
+++ catalog_and_checkout.js
@@ -1,20 +1,21 @@
 /* Candle Class */
-function product(name, cost, qty){
+function product(name, cost, qty, description){
     this.name = name;
     this.cost = cost;
     this.quantity = qty;
+    this.description = description;
 }
 
 catalog = new Array (
-    new product("Tranquil Twilight", 12, 0),
-    new product("Citrus Serenity", 15, 0),
-    new product("Enchanted Embers", 10, 0),
-    new product("Midnight Jasmine", 14, 0),
-    new product("Cozy Cabin Glow", 12, 0),
-    new product("Ocean Breeze Bliss", 15, 0),
-    new product("Vanilla Velvet Dream", 14, 0),
-    new product("Fireside Harmony", 10, 0),
-    new product("Lavender Lullaby", 12, 0)
+    new product("Tranquil Twilight", 12, 0, "This candle offers a soothing blend of lavender and chamomile scents, perfect for winding down after a long day. Let its gentle fragrance envelop you in tranquility."),
+    new product("Citrus Serenity", 15, 0, "Enjoy the refreshing and invigorating aroma of zesty citrus fruits. This candle brings a burst of energy to any room, creating a sense of serenity and awakening your senses."),
+    new product("Enchanted Embers", 10, 0, "Enchanted Embers evokes the cozy ambiance of a crackling fireplace. Its warm and woody notes create a magical atmosphere, perfect for a relaxing evening."),
+    new product("Midnight Jasmine", 14, 0, "The fragrance of midnight jasmine fills the air with floral elegance and a touch of mystery. Light this candle to set a romantic and enchanting mood."),
+    new product("Cozy Cabin Glow", 12, 0, "Imagine the warmth of a cozy cabin in the woods. This candle combines rustic woodsy scents with a hint of spiced vanilla, providing a comforting and inviting atmosphere."),
+    new product("Ocean Breeze Bliss", 15, 0, "Bring the soothing scent of the ocean into your home. Ocean Breeze Bliss offers a fresh and revitalizing fragrance, reminiscent of a day at the beach."),
+    new product("Vanilla Velvet Dream", 14, 0, "Dive into a dreamy world of creamy vanilla. This candle envelops you in a warm and comforting embrace, like a velvety dessert for the senses."),
+    new product("Fireside Harmony", 10, 0, "Fireside Harmony captures the essence of a crackling campfire. Its smoky and earthy scents create a harmonious ambiance that brings people together."),
+    new product("Lavender Lullaby", 12, 0, "Lavender Lullaby is the perfect candle to help you relax and unwind. With its calming lavender scent, it lulls you into a peaceful and restful sleep.")
 );
 
 function createCatalog(){
@@ -43,12 +44,18 @@ $(document).ready(function(){
 
         currProduct = catalog.find(product => product.name === $(this).text());
         $(".modal-inner h2").text(currProduct.name);
+        $(".modal-inner p").text(currProduct.description)
         
     })
 
     $("#closeModal").on("click", function(){
         $("#modal").removeClass("open");
     })
+
+    // $(".modal").on("click", function(){
+    //     $("#modal").removeClass("open");
+
+    // })
 
 
 
